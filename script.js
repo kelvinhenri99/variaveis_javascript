e399a5e2bb7417e3d622bdc5d6a2c17f47e351dd
kelvinhenri99 <kelvin5henri@gmail.com>
2022-04-05 10:38:33 -0300

    Implementação do código

---
+++ script.js
@@ -0,0 +1,13 @@
+//Novo formato para variável LET
+
+var qtd = 10;
+var valor = 1.50;
+var nome = "Caneta";
+var total = valor * qtd;
+
+
+
+console.log("Produto: " + nome);
+console.log("Quantidade: " + qtd);
+console.log("Valor: ", "R$" + valor);
+console.log("Total: " + total);
